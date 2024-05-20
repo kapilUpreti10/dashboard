52a52b1c1f2ff49cf164f1c4f8d8055126ee197a
Kapil Upreti <kapilupretiff@gmail.com>
2024-05-21 00:42:39 +0545

    dashboard edited

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -24,8 +24,6 @@ import {
   Badge,
   ShoppingCart,
   Package,
-  Users,
-  LineChart,
   Search,
   CircleUser,
   Home,
@@ -41,7 +39,7 @@ const HomePage = () => {
             <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
               <Link to="/" className="flex items-center gap-2 font-semibold">
                 <Package2 className="h-6 w-6" />
-                <span className="">Acme Inc</span>
+                <span className="">Kapil's Book Store</span>
               </Link>
               <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
                 <Bell className="h-4 w-4" />
@@ -55,7 +53,7 @@ const HomePage = () => {
                   className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
                 >
                   <Home className="h-4 w-4" />
-                  Dashboard
+                  Home
                 </Link>
                 <Link
                   to="#"
@@ -72,39 +70,9 @@ const HomePage = () => {
                   className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
                 >
                   <Package className="h-4 w-4" />
-                  Products{" "}
-                </Link>
-                <Link
-                  to="#"
-                  className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                >
-                  <Users className="h-4 w-4" />
-                  Customers
-                </Link>
-                <Link
-                  to="#"
-                  className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                >
-                  <LineChart className="h-4 w-4" />
-                  Analytics
+                  Books
                 </Link>
               </nav>
-            </div>
-            <div className="mt-auto p-4">
-              <Card x-chunk="dashboard-02-chunk-0">
-                <CardHeader className="p-2 pt-0 md:p-4">
-                  <CardTitle>Upgrade to Pro</CardTitle>
-                  <CardDescription>
-                    Unlock all features and get unlimited access to our support
-                    team.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
-                  <Button size="sm" className="w-full">
-                    Upgrade
-                  </Button>
-                </CardContent>
-              </Card>
             </div>
           </div>
         </div>
@@ -152,39 +120,9 @@ const HomePage = () => {
                     className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
                   >
                     <Package className="h-5 w-5" />
-                    Products
-                  </Link>
-                  <Link
-                    to="#"
-                    className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                  >
-                    <Users className="h-5 w-5" />
-                    Customers
-                  </Link>
-                  <Link
-                    to="#"
-                    className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                  >
-                    <LineChart className="h-5 w-5" />
-                    Analytics
+                    Books
                   </Link>
                 </nav>
-                <div className="mt-auto">
-                  <Card>
-                    <CardHeader>
-                      <CardTitle>Upgrade to Pro</CardTitle>
-                      <CardDescription>
-                        Unlock all features and get unlimited access to our
-                        support team.
-                      </CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                      <Button size="sm" className="w-full">
-                        Upgrade
-                      </Button>
-                    </CardContent>
-                  </Card>
-                </div>
               </SheetContent>
             </Sheet>
             <div className="w-full flex-1">
