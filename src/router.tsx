849a6e331e369056eede83f7ec245115d52460be
Kapil Upreti <kapilupretiff@gmail.com>
2024-05-20 23:13:49 +0545

    router added

---
+++ src/router.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import  Login  from "./pages/Login";
+import Home from "./pages/Home";
+
+
+const Router = createBrowserRouter([
+
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    }
+]
+);
+
+export default Router;
