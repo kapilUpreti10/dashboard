e340c6d81967b91f2f194a46d7a050b28be3198a
Kapil Upreti <kapilupretiff@gmail.com>
2024-05-23 15:15:35 +0545

    dashboard edited

---
+++ src/pages/BooksList.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BooksPage = () => {
+  return (
+    <div>
+      <p>this is about page</p>
+    </div>
+  );
+};
+
+export default BooksPage;
--- src/router.tsx
+++ src/router.tsx
@@ -2,11 +2,23 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "./pages/Login";
 import HomePage from "./pages/Home";
 import Signup from "./pages/Signup";
+import BooksPage from "./pages/BooksList";
+import DashboardLayout from "./layouts/DashboardLayout";
 
 const Router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/books",
+        element: <BooksPage />,
+      },
+    ],
   },
   {
     path: "/login",
